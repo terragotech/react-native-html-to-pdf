2325e1e22bf5e5abe33d7cc9e27c75e6772b849b
ram <ramachandran@knetworks.co.in>
2018-02-21 14:45:20 +0530

    import added

--- android/src/main/java/com/christopherdro/htmltopdf/RNHTMLtoPDFModule.java
+++ android/src/main/java/com/christopherdro/htmltopdf/RNHTMLtoPDFModule.java
@@ -13,6 +13,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 import java.util.HashSet;
 import java.util.Set;
